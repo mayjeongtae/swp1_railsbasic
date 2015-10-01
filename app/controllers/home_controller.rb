a5769ac962569941a50c0b834aa3f1a8778a326b
mayjeongtae <mayjeongtae@snu.ac.kr>
2015-10-01 08:07:56 +0000

    rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def why
+    end
+    
+    def who
+    end
+    
+    def when
+    end
+end
